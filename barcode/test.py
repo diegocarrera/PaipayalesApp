60e50a326a30189ad2c84868df8e777e28967552
mdleiton <mau_leiton96@hotmail.com>
2019-04-09 23:08:25 -0500

    [add] generacion de codigo de barras

---
+++ barcode/test.py
@@ -0,0 +1,11 @@
+import barcode
+CANTIDAD = 10
+print(barcode.PROVIDED_BARCODES)
+ean13 = barcode.get_barcode_class('ean13')
+for i in range(CANTIDAD):
+    code = str(i)
+    x = ["0" for cero in range(12-len(code)) ]
+    id = "".join(x) + code
+    codigo = ean13(id)
+    codigo.save(id)
+
