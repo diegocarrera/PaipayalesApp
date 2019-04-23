ca9b43df1598aa69ac4eddf8cafe69bec9fcce2a
mdleiton <mau_leiton96@hotmail.com>
2019-04-23 03:01:28 -0500

    [upd] se obtienen la lista de puntos de reparto

--- PaipayApp/app/src/main/java/ec/edu/espol/cvr/paipayapp/ConfigPuntoRepartoAdmin.java
+++ PaipayApp/app/src/main/java/ec/edu/espol/cvr/paipayapp/ConfigPuntoRepartoAdmin.java
@@ -10,6 +10,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,12 +25,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ec.edu.espol.cvr.paipayapp.utils.Invariante;
-import ec.edu.espol.cvr.paipayapp.utils.RequestApi;
 
 public class ConfigPuntoRepartoAdmin extends Activity implements AdapterView.OnItemSelectedListener {
     private Spinner spinner;
     private SharedPreferences sharedpreferences;
     List<String> puntos_reparto = new ArrayList<String>();
+    private boolean test_mode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +39,16 @@ public class ConfigPuntoRepartoAdmin extends Activity implements AdapterView.OnI
         sharedpreferences = getSharedPreferences(Invariante.MyPREFERENCES, this.MODE_PRIVATE);
         spinner = (Spinner) findViewById(R.id.spinnerPuntoReparto);
         spinner.setOnItemSelectedListener(this);
-        update_option();
-        String punto_reparto = sharedpreferences.getString("punto_reparto","");
+        test_mode = sharedpreferences.getBoolean("test_mode",true);
+        if(test_mode){
+            Toast.makeText(this, Invariante.PRUEBA, Toast.LENGTH_SHORT).show();
+            puntos_reparto.add("Principal, Paipay");
+            puntos_reparto.add("Norte");
+            set_option();
+        }else{
+            update_option();
+        }
+        String punto_reparto = sharedpreferences.getString(Invariante.PUNTO_REPARTO,"");
         if(punto_reparto != ""){
             spinner.setSelection(puntos_reparto.indexOf(punto_reparto));
         }
@@ -44,45 +59,62 @@ public class ConfigPuntoRepartoAdmin extends Activity implements AdapterView.OnI
         String item = parent.getItemAtPosition(position).toString();
         Toast.makeText(parent.getContext(), "Punto de reparto seleccionado: " + item, Toast.LENGTH_LONG).show();
         SharedPreferences.Editor editor = sharedpreferences.edit();
-        editor.putString("punto_reparto", item);
+        editor.putString(Invariante.PUNTO_REPARTO, item);
         editor.apply();
     }
     public void onNothingSelected(AdapterView<?> arg0) {
         // TODO Auto-generated method stub
     }
 
+    public void set_option(){
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(ConfigPuntoRepartoAdmin.this, R.layout.spinner, puntos_reparto);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    }
+
     public void update_option(){
         String ip = sharedpreferences.getString("ip","");
         int port = sharedpreferences.getInt("port",0);
-        boolean test_mode = sharedpreferences.getBoolean("test_mode",true);
-        if(ip!= "" && port != 0){
-            RequestApi.set_network(ip, port);
-            JSONObject response = new JSONObject();
-            try {
-                response = RequestApi.request("/api/v1/puntos_reparto/", "GET", null);
-                if(response.getInt("response_code") == 200){
-                    JSONObject puntos = new JSONObject(response.getString("data"));
-                    JSONArray jsonarr = puntos.getJSONArray("puntos_reparto");
-                    for (int i = 0; i < jsonarr.length(); i++) {
-                        puntos_reparto.add( jsonarr.getString(i));
-                    }
-                }else{
-                    Toast.makeText(this, response.getString("error"), Toast.LENGTH_SHORT).show();
-                    if (test_mode){
-                        puntos_reparto.add("Principal, Paipay");
-                        puntos_reparto.add("Norte");
-                    }
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }else{
-            Toast.makeText(this, "IP y/o puerto del servidor no configurado. ", Toast.LENGTH_LONG).show();
-        }
 
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.spinner, puntos_reparto);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(dataAdapter);
+        if(ip!= "" && port != 0){
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            String server = Invariante.get_server(ip, port);
+            JsonArrayRequest jsonObjectRequest = new JsonArrayRequest
+                    (Request.Method.GET,server + "/api/v1/delivery-centers/" , null, new Response.Listener<JSONArray>() {
+
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            try {
+                                for (int i = 0; i < response.length(); i++) {
+                                    JSONObject centro = response.getJSONObject(i);
+                                    String name = String.valueOf(centro.getInt("id")) + "-" + centro.getString("name");
+                                    puntos_reparto.add(name);
+                                }
+                                set_option();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                Toast.makeText(ConfigPuntoRepartoAdmin.this, Invariante.ERROR_LOGIN_RED, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            try {
+                                int code = error.networkResponse.statusCode;
+                                JSONObject json = new JSONObject(new String(error.networkResponse.data));
+                                String message = "Error " + String.valueOf(code) + json.getString("message");
+                                Toast.makeText(ConfigPuntoRepartoAdmin.this, message, Toast.LENGTH_SHORT).show();
+                            }catch (Exception e) {
+                                e.printStackTrace();
+                                Toast.makeText(ConfigPuntoRepartoAdmin.this, Invariante.ERROR_LOGIN_RED_ACCESO, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+            requestQueue.add(jsonObjectRequest);
+        }else{
+            Toast.makeText(this, Invariante.CONF_ERROR_1, Toast.LENGTH_LONG).show();
+        }
     }
 
     public void onBackPressed() {
